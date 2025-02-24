1813656303a20dcd2808f6c432d5708ad5ef7d02
keikei014 <fjac014@gmail.com>
2025-02-24 17:22:41 +0100

    field filling debugging

--- src/betaflight_platform.cpp
+++ src/betaflight_platform.cpp
@@ -397,9 +397,10 @@ void BetaflightPlatform::onMotor(const msp::msg::Motor & motor)
 {
   std::cout << "Motor: " << motor << std::endl;
   std_msgs::msg::UInt16MultiArray debug_motor_msg;
-  debug_motor_msg.layout.dim.resize(1);
-  debug_motor_msg.layout.dim[0].size = 8;
-  debug_motor_msg.data.reserve(8);
+  std::cout << "Motor layout:" << std::endl;
+  debug_motor_msg.layout.dim.reserve(1);
+  debug_motor_msg.layout.dim[0].size = motor.motor.size();
+  // debug_motor_msg.data.resize(8);
   // debug_motor_msg.data[0] = motor.motor[0];
   // debug_motor_msg.data[1] = motor.motor[1];
   // debug_motor_msg.data[2] = motor.motor[2];
@@ -408,12 +409,21 @@ void BetaflightPlatform::onMotor(const msp::msg::Motor & motor)
   // debug_motor_msg.data[5] = motor.motor[5];
   // debug_motor_msg.data[6] = motor.motor[6];
   // debug_motor_msg.data[7] = motor.motor[7];
-  std::cout << "Motor data stored for M1:" << debug_motor_msg.data[0] << std::endl;
-  debug_motors_pub_->publish(debug_motor_msg);
+  std::cout << "Motor reserve:" << std::endl;
+  debug_motor_msg.data.reserve(motor.motor.size());
+  // std::cout << "Motor:" << std::endl;
+  // for (int i = 0; i < 8; i++) {
+  //   std::cout << i << " " << motor.motor[i] << std::endl;
+  //   debug_motor_msg.data.push_back(motor.motor[i]);
+  // }
 
   for (auto motor_value:motor.motor) {
+    std::cout << "Value to be stored: " << motor_value << std::endl;
     debug_motor_msg.data.emplace_back(motor_value);
   }
+
+  std::cout << "Motor data stored for M1:" << debug_motor_msg.data[0] << std::endl;
+  debug_motors_pub_->publish(debug_motor_msg);
 }
 
 void BetaflightPlatform::onBattery(const msp::msg::BatteryState & battery)
