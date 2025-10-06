82cb0b889559a6fa30ba0ed527448ef458dc17d3
fjanguita <fj.anguita@upm.es>
2025-10-06 19:35:32 +0200

    Config acc_1G for btfl 4.4.0 version.

--- src/betaflight_platform.cpp
+++ src/betaflight_platform.cpp
@@ -366,7 +366,7 @@ void BetaflightPlatform::onImu(const msp::msg::RawImu & imu)
 
   // from Betaflight MPU6000 drivers init: acc_1G = 512.0 * 4
   // const msp::msg::ImuSI imu_si(imu, 512.0 * 4, 1.0 / 4.096, 0.092, 9.80665);
-  const double acc_1G = 512.0f * 4.0f;
+  const double acc_1G = 512.0f;
   const double gyro_scale = 1.0f / 16.0f;
   const msp::msg::ImuSI imu_si(imu, acc_1G, gyro_scale, 0.092, 9.80665);
 
